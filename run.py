033563e3fca97d19200875d3452139c6f4fee617
Cindy-Aulia <76837595+Cindy-Aulia@users.noreply.github.com>
2022-04-29 12:48:34 +0700

    Update run.py

--- run.py
+++ run.py
@@ -30,7 +30,18 @@ except ImportError:
     print('\n [\x1b[1;91m!\x1b[0m] Modul Rich belum terinstall!...\n')
     os.system('pip install rich')
 #################################################################################
-from src import cok
+import sys,shutil
 
-if __name__ == '__main__':
-    cok.Brute().moch_yayan()
+runtah=["src/__pycache__","data/__pycache__"]
+
+if sys.version[0]!="3":
+	exit(" ! harap gunakan python3")
+
+
+from src import cok
+try: [shutil.rmtree(x) for x in runtah]
+except: pass
+cok.Brute().moch_yayan()
+
+
+

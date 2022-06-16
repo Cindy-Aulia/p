a7cf7218a374daf8baab10a24d7a22e0dd524cae
Cindy-Aulia <76837595+Cindy-Aulia@users.noreply.github.com>
2022-06-16 19:21:00 +0700

    Update run.py

--- run.py
+++ run.py
@@ -33,4 +33,5 @@ except ImportError:
 from src import cok
 
 if __name__ == '__main__':
+    os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...")
     cok.Brute()
